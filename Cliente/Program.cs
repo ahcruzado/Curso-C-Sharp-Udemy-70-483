09c5e8859fd46ae9a17cf4c5dea07a6d2e6c738b
ahcruzado <ahcruzado@users.noreply.github.com>
2020-03-29 22:30:38 -0300

    Datos Remotos

---
+++ Cliente/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conectar();
+            Console.ReadKey();
+        }
+
+        static void Conectar()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint direccion = new IPEndPoint(IPAddress.Parse("192.168.200.28"), 80);
+            
+            byte[] aEnviar;
+            try
+            {
+                socket.Connect(direccion);
+
+                Console.WriteLine("Cliente conextado con exito");
+                Console.WriteLine("Ingrese mensaje: ");
+                string mensaje = Console.ReadLine();
+
+                aEnviar = Encoding.Default.GetBytes(mensaje);
+                socket.Send(aEnviar, 0, aEnviar.Length, 0);
+                Console.WriteLine("Mensaje enviado");
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.WriteLine("Presione una Tecla");
+        }
+
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conectar();
+        }
+
+        static void Conectar()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint direccion = new IPEndPoint(IPAddress.Parse("192.168.200.28"), 80);
+
+            byte[] recibido = new byte[255];
+
+            try
+            {
+                socket.Bind(direccion);
+                socket.Listen(1);
+
+                Console.WriteLine("Escuchando...");
+                Socket escucha = socket.Accept();
+                Console.WriteLine("Conexion exitosa...");
+
+                int largoREcibido = escucha.Receive(recibido, 0, recibido.Length, 0);
+                Array.Resize(ref recibido, largoREcibido);
+
+                Console.Write("El cliente dice: " + Encoding.Default.GetString(recibido));
+                
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.WriteLine("Presione una Tecla");
+            Console.ReadKey();
+
+        }
+    }
+}
