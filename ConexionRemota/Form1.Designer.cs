86972e81e3dc2168381407bbb26c8c8097c41606
ahcruzado <ahcruzado@users.noreply.github.com>
2020-03-29 21:51:27 -0300

    no message

---
+++ ConexionRemota/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ConexionRemota
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Urllabel = new System.Windows.Forms.Label();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.RichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.AceptarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Urllabel
+            // 
+            this.Urllabel.AutoSize = true;
+            this.Urllabel.Location = new System.Drawing.Point(11, 14);
+            this.Urllabel.Name = "Urllabel";
+            this.Urllabel.Size = new System.Drawing.Size(29, 13);
+            this.Urllabel.TabIndex = 0;
+            this.Urllabel.Text = "URL";
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.Location = new System.Drawing.Point(52, 11);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(337, 20);
+            this.UrlTextBox.TabIndex = 1;
+            // 
+            // RichTextBox1
+            // 
+            this.RichTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RichTextBox1.Location = new System.Drawing.Point(10, 65);
+            this.RichTextBox1.Name = "RichTextBox1";
+            this.RichTextBox1.Size = new System.Drawing.Size(379, 242);
+            this.RichTextBox1.TabIndex = 2;
+            this.RichTextBox1.Text = "";
+            // 
+            // AceptarButton
+            // 
+            this.AceptarButton.Location = new System.Drawing.Point(315, 37);
+            this.AceptarButton.Name = "AceptarButton";
+            this.AceptarButton.Size = new System.Drawing.Size(75, 23);
+            this.AceptarButton.TabIndex = 3;
+            this.AceptarButton.Text = "Aceptar";
+            this.AceptarButton.UseVisualStyleBackColor = true;
+            this.AceptarButton.Click += new System.EventHandler(this.AceptarButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 320);
+            this.Controls.Add(this.AceptarButton);
+            this.Controls.Add(this.RichTextBox1);
+            this.Controls.Add(this.UrlTextBox);
+            this.Controls.Add(this.Urllabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Urllabel;
+        private System.Windows.Forms.TextBox UrlTextBox;
+        private System.Windows.Forms.RichTextBox RichTextBox1;
+        private System.Windows.Forms.Button AceptarButton;
+    }
+}
+
---
+++ ConexionRemota/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConexionRemota
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void RecuperarDatos(string url)
+        {
+            WebClient webClient = new WebClient();
+            Stream stream= webClient.OpenRead(url);
+
+            StreamReader reader = new StreamReader(stream);
+            string respuesta = reader.ReadToEnd();
+            reader.Close();
+
+            this.RichTextBox1.Text = respuesta;
+
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            RecuperarDatos(UrlTextBox.Text);
+        }
+    }    
+}
