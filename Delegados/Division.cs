3d148aa5211672f8b3dee0bf508d1b541e498c90
ahcruzado <ahcruzado@users.noreply.github.com>
2020-04-01 21:33:31 -0300

    no message

---
+++ Delegados/Division.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegados
+{
+    public class Division
+    {
+        public Action<int, int> MessageHanlder;
+        public int Divide(int a, int b)
+        {
+            int result = 0;
+            if (b==0)
+            {
+                MessageHanlder(a, b);
+            }
+            else
+            {
+                result= a / b;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Delegados/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegados
+{
+    class Program
+    {
+
+        delegate void OperacionDelegate();
+        delegate int OperacionDelegate2(int x, int b);
+
+        static void Main(string[] args)
+        {
+            Sample01();
+            Console.WriteLine(Environment.NewLine);
+
+            Sample02();
+            Console.WriteLine(Environment.NewLine);
+
+            Sample03();
+            Console.WriteLine(Environment.NewLine);
+
+            Sample04();
+            Console.WriteLine(Environment.NewLine);
+
+            Sample05();
+            Console.WriteLine(Environment.NewLine);
+
+            Console.ReadKey();
+        }
+
+        private static void Sample01()
+        {
+            OperacionDelegate dlg = Message;
+            dlg();
+        }
+
+        private static void Sample02()
+        {
+            OperacionDelegate2 dlg = Message;
+
+            Console.WriteLine(dlg(2,2));
+        }
+
+        private static void Sample03()
+        {
+            Action a = ActionMessage;
+            a();
+        }
+
+        private static void Sample04()
+        {
+            Func<int, int, int> f = Sumar;
+            Console.WriteLine(f(1,5));
+        }
+
+        private static void Sample05()
+        {
+            string a, b;
+            int result = 0;
+
+            Division op = new Division();
+            op.MessageHanlder = ShowMessage;
+
+            Console.WriteLine("Ingrese el Operador 1");
+            a = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el Operador 2");
+            b = Console.ReadLine();
+
+            result = op.Divide(Convert.ToInt32(a), Convert.ToInt32(b));
+            Console.WriteLine($"Resultado: {result}");
+        }
+
+        public static void ShowMessage(int a, int b)
+        {
+            Console.WriteLine($"División entre cero, {a} - {b}");
+        }
+
+        private static void ActionMessage()
+        {
+            Console.WriteLine("Mensaje Action");
+
+        }
+
+        static void Message()
+        {
+            Console.Write("Un mensaje");
+        }
+
+        static int Message(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
