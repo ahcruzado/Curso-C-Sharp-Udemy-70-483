447dec85239627940aa9847f7fefd1cf8945d29b
ahcruzado <ahcruzado@users.noreply.github.com>
2020-04-04 19:45:41 -0300

    Async Await

---
+++ AyncAwait/Archivos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AyncAwait
+{
+    public class Archivos
+    {
+        public Task<List<string>> ListarInformacion()
+        {
+            return Task.Run(() =>
+            {
+                var files = from file in Directory.GetFiles(@"C:\Windows\System32")
+                            select file;
+
+                Thread.Sleep(5000);
+                return files.ToList();
+            });            
+        }
+    }
+}
---
+++ Task01/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task01
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Sample01();
+            Sample02();
+        }
+
+        public void Sample01()
+        {
+            Task t1 = new Task(() => MessageBox.Show("mensaje"));
+            t1.Start();
+
+            AddMessage("Final de hila principal",0);
+        }
+
+        public void Sample02()
+        {
+            Task t1 = new Task(() => MessageBox.Show("mensaje"));
+            t1.Start();
+
+            Task t2 = new Task(() => AddMessage("Ejecutando tarea 2", 5));
+            t2.Start();
+
+            Task t3 = new Task(() => AddMessage("Ejecutando tarea 3", 5));
+            t3.Start();
+
+            Task t4 = new Task(() => AddMessage("Ejecutando tarea 4", 5));
+            t4.Start();
+
+            AddMessage("Final de hila principal", 0);
+        }
+
+        void AddMessage(string message, int seconds)
+        {
+            Thread.Sleep(1000 * seconds);
+
+            var hilo = Thread.CurrentThread.ManagedThreadId;
+
+            this.Dispatcher.Invoke(() =>
+                                    {
+                                        MessageLabel.Content += $"Mensaje: {message}, " +
+                                                                $"Hola Actual:{hilo} \n ";
+                                    });
+
+
+        }
+    }
+}
