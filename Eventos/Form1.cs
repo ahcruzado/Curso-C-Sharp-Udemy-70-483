7a6e4d638b68e777f9e811b7464b46cd2f7855ac
ahcruzado <ahcruzado@users.noreply.github.com>
2020-04-04 18:49:00 -0300

    no message

---
+++ Eventos/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eventos
+{
+    public partial class Form1 : Form
+    {
+
+        public delegate void CambiarSaldoEventHandler(string cliente, string cuenta, double saldo);
+
+        public event CambiarSaldoEventHandler CambiarSaldo;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.CambiarSaldo += MetodoGestor;
+        }
+
+        private void DepositarButton_Click(object sender, EventArgs e)
+        {
+            Depositar(double.Parse(ImporteTextBox.Text));
+        }
+
+        private void RetirarButton_Click(object sender, EventArgs e)
+        {
+            Retirar(double.Parse(ImporteTextBox.Text));
+        }
+
+        double Saldo;
+        public void Depositar(double cantidad)
+        {
+            if (cantidad > 0)
+            {
+                Saldo = double.Parse(SaldoTextBox.Text) + cantidad;
+                CambiarSaldo(ClienteTextBox.Text, CuentaTextBox.Text, Saldo);
+            }
+            else
+                MessageBox.Show("Cantidad Inválida");
+        }
+
+        public void Retirar(double cantidad)
+        {
+            if (double.Parse(SaldoTextBox.Text) > cantidad)
+            {
+                Saldo = double.Parse(SaldoTextBox.Text) -cantidad;
+            }
+            else
+                MessageBox.Show("Saldo insuficiente");
+        }
+
+        public void MetodoGestor(string cliente, string cuenta, double saldo)
+        {
+            string message = "Estimado(a) {0} \nSe le notifica que el saldo de su cuenta {1} se ha modificado\nNuevo Saldo: {2}";
+            message = string.Format(message, cliente, cuenta, saldo.ToString("C"));
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ MetodosAnonimos/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosAnonimos
+{
+    class Program
+    {
+        delegate void ShowMessageDelegate();
+
+        static void Main(string[] args)
+        {
+            Sample01();
+            Sample02();
+            Sample03();
+            Sample04();
+            Sample05();
+            Sample06();
+            Console.ReadKey();
+        }
+
+        static void Sample01()
+        {
+            Action E = ShowMessage;
+            E();
+
+        }
+
+        static void Sample02()
+        {
+            Action D = delegate ()
+            {
+                Console.WriteLine("Mensaje Método anonimo");
+            };
+
+            D();
+        }
+
+        static void Sample03()
+        {
+            ShowMessageDelegate D = delegate ()
+            {
+                Console.WriteLine("Mensaje Método anonimo con delegado");
+            };
+
+            D();
+        }
+
+        static void Sample04()
+        {
+            Action<string> s;
+
+            s = delegate (string message)
+              {
+                  Console.WriteLine(message);
+              };
+
+            s("mensaje con delegado parametrizado");
+        }
+
+        static void Sample05()
+        {
+            Func<int, int, int> F = Add;
+            var result = F(2, 4);
+            Console.WriteLine(result);
+        }
+
+        static void Sample06()
+        {
+            Func<int, int, int> F = delegate (int a, int b)
+            {
+                return a + b;
+            };
+
+            var result=F(2, 4);
+            Console.WriteLine(result);
+        }
+
+        static void ShowMessage()
+        {
+            Console.WriteLine("Messaje");
+        }
+
+        static void ParametrizedShowMessage(string messag)
+        {
+            Console.WriteLine("Messaje");
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
